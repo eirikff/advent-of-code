d9b2e723e755fa37e6e6c5d2a0a0470f0e759290
eirikff <1403257+eirikff@users.noreply.github.com>
2024-12-20 10:47:37 +0100

    Finish day 19 part 2 and refactor day 1

--- 2024/des19/solve.py
+++ 2024/des19/solve.py
@@ -17,30 +17,27 @@ def solve_part1(options: set[str], patterns: list[str]) -> int:
     max_length = max(lengths)
 
     memo = {}
-    def check_pattern(pattern: str, depth: int = 0) -> bool:
-        if (pattern, depth) in memo:
-            return memo[(pattern, depth)]
+    def check_pattern(pattern: str) -> bool:
+        if pattern in memo:
+            return memo[pattern]
 
         # We reached the end of the pattern meaning we found a match for each
         # substring
         if len(pattern) == 0:
             return True
 
-        for i in range(len(pattern)):
-            for j in range(min_length, max_length+1):
-                sub = pattern[i:i+j]
-                if sub in options:
-                    success = check_pattern(pattern[j:], depth + 1)
-                    if success:
-                        memo[(pattern, depth)] = True
-                        return True
+        for j in range(min_length, max_length+1):
+            sub = pattern[:j]
+            if sub in options:
+                success = check_pattern(pattern[j:])
+                if success:
+                    memo[pattern] = True
+                    return True
 
-            # If we can't match the start of the pattern, it automatically
-            # fails
-            memo[(pattern, depth)] = False
-            return False
-
-        raise RuntimeError(f"This should never be reached. {pattern=}, {options=}")
+        # If we can't match the start of the pattern, it automatically
+        # fails
+        memo[pattern] = False
+        return False
 
 
     total_count = 0
@@ -52,65 +49,30 @@ def solve_part1(options: set[str], patterns: list[str]) -> int:
 
 
 def solve_part2(options: set[str], patterns: list[str]) -> int:
-    lengths = list(map(len, options))
-    min_length = min(lengths)
-    max_length = max(lengths)
-
-    # def print(*args): pass
-
     memo = {}
-    def find_combination(pattern: str, 
-                         exclude_path: set[tuple] = set(), 
-                         path: tuple = (), 
-                         depth: int = 0) -> tuple[bool,tuple]:
-        # Must use frozenset to make the exclude_path hashable
-        arg_tuple = (pattern, frozenset(exclude_path), path, depth)
-        if arg_tuple in memo:
-            return memo[arg_tuple]
-
+    def find_count(pattern: str, path: tuple = ()) -> int:
         # We reached the end of the pattern meaning we found a match for each
         # substring
         if len(pattern) == 0:
-            return True, path
+            return 1
 
-        for i in range(len(pattern)):
-            for j in range(min_length, max_length+1):
-                sub = pattern[i:i+j]
-                if sub in options:
-                    success, fullpath = find_combination(
-                        pattern[j:], exclude_path, (*path, sub), depth + 1)
-                    if success and fullpath not in exclude_path:
-                        # print(f"[{depth}] {len(memo)}")
-                        memo[arg_tuple] = (success, fullpath)
-                        return success, fullpath
+        if pattern in memo:
+            return memo[pattern]
 
-            memo[arg_tuple] = (False, ())
-            return False, ()
+        count = 0
+        for sub in options:
+            if pattern.startswith(sub):
+                j = len(sub)
+                count += find_count(pattern[j:], (*path, sub))
 
-        raise RuntimeError(f"This should never be reached. {pattern=}, {options=}, {exclude_path=}")
-
-
-    def find_all(pattern: str) -> set[tuple]:
-        paths = set()
-        while True:
-            success, path = find_combination(pattern, paths)
-            if success:
-                # print("found full path", path)
-                paths.add(path)
-            else:
-                # print("impossible")
-                break
-
-        return paths
+        memo[pattern] = count
+        return count
 
 
     total_count = 0
     for pattern in patterns:
-        # print("\nTESTING PATTERN", pattern, "OPTIONS", options)
-        print(pattern, end="", flush=True)
-        paths = find_all(pattern)
-        print("\tcount:", len(paths))
-        total_count += len(paths)
+        count = find_count(pattern)
+        total_count += count
 
     return total_count
 
